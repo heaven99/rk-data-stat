168d6621df9dacd2d1658355de0a99d236e1a8b9
SJ-Kim <sjkim@ckstack.com>
2026-01-05 14:21:24 +0900

    etc : update db scheme

--- _assets/sql/ck_history_db/create_table.sql
+++ _assets/sql/ck_history_db/create_table.sql
@@ -1,4 +1,6 @@
 DROP TABLE public.tbl_device_his_inf;
+
+-- create table
 CREATE TABLE public.tbl_device_his_inf (
     id         BIGINT GENERATED ALWAYS AS IDENTITY,
     rid        CHAR(32),
@@ -12,6 +14,7 @@ CREATE TABLE public.tbl_device_his_inf (
     c_date     TIMESTAMP
 );
 
+-- add comment for column
 COMMENT ON TABLE public.tbl_device_his_inf IS 'device history information raw log table';
 COMMENT ON COLUMN public.tbl_device_his_inf.id IS 'surrogate key (identity), primary key will be added later';
 COMMENT ON COLUMN public.tbl_device_his_inf.rid IS 'request or relation id';
@@ -24,9 +27,10 @@ COMMENT ON COLUMN public.tbl_device_his_inf.extra IS 'extra json payload (jsonb)
 COMMENT ON COLUMN public.tbl_device_his_inf.orig_data IS 'original raw payload data';
 COMMENT ON COLUMN public.tbl_device_his_inf.c_date IS 'log created datetime (timestamp without timezone)';
 
-
+-- add index
 CREATE INDEX idx_tbl_device_his_inf_c_date_serial_num ON public.tbl_device_his_inf (c_date, serial_num);
 
+-- add comment for index
 COMMENT ON INDEX public.idx_tbl_device_his_inf_c_date_serial_num IS 'index for device history query by c_date, serial_num';
 
 -- add pk
--- _assets/sql/ck_stat_db/create_table.sql
+++ _assets/sql/ck_stat_db/create_table.sql
@@ -1,6 +1,7 @@
 -- stat src
 -- DROP TABLE `ck_stat_db`.`tbl_stat_src`;
 -- TODO partition
+-- create table
 CREATE TABLE public.tbl_stat_src (
     stat_date  CHAR(14) NOT NULL,
     serial_num CHAR(17) NOT NULL,
@@ -8,15 +9,18 @@ CREATE TABLE public.tbl_stat_src (
     value      INTEGER
 );
 
--- add comment
+-- add comment for column
 COMMENT ON TABLE public.tbl_stat_src IS 'stat source raw table';
 COMMENT ON COLUMN public.tbl_stat_src.stat_date IS 'statistic date. YYYYMMDDHHmmss';
 COMMENT ON COLUMN public.tbl_stat_src.serial_num IS 'boiler serial number';
 COMMENT ON COLUMN public.tbl_stat_src.data_type IS 'statistic type';
 COMMENT ON COLUMN public.tbl_stat_src.value IS 'statistic value';
+
 -- TODO index query 별도 분리
+-- ad index
 -- 전체 장비도 조회할 수 있으니 일단 순서를 data_type 에 serial_num 으로 변경
 CREATE INDEX idx_tbl_stat_src_stat_date_data_type_serial_num ON public.tbl_stat_src (stat_date, data_type, serial_num);
 
+-- add comment for index
 COMMENT ON INDEX public.idx_tbl_stat_src_stat_date_data_type_serial_num IS 'index for statistic query by stat_date, data_type, serial_num';
 
