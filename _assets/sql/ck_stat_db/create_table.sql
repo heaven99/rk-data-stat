516091145963dd68a974a890205db7f1939a0094
SJ-Kim <sjkim@ckstack.com>
2026-01-06 17:50:22 +0900

    etc : tbl_stat_src 테이블 스키마 수정

--- _assets/sql/ck_stat_db/create_table.sql
+++ _assets/sql/ck_stat_db/create_table.sql
@@ -1,12 +1,12 @@
 -- stat src
--- DROP TABLE `ck_stat_db`.`tbl_stat_src`;
+DROP TABLE public.tbl_stat_src;
 -- TODO partition
 -- create table
 CREATE TABLE public.tbl_stat_src (
     stat_date  CHAR(14) NOT NULL,
     serial_num CHAR(17) NOT NULL,
     data_type  VARCHAR(16) NOT NULL,
-    value      INTEGER
+    value      DOUBLE PRECISION
 );
 
 -- add comment for column
@@ -19,8 +19,7 @@ COMMENT ON COLUMN public.tbl_stat_src.value IS 'statistic value';
 -- TODO index query 별도 분리
 -- ad index
 -- 전체 장비도 조회할 수 있으니 일단 순서를 data_type 에 serial_num 으로 변경
-CREATE INDEX idx_tbl_stat_src_stat_date_data_type_serial_num ON public.tbl_stat_src (stat_date, data_type, serial_num);
+CREATE INDEX idx_tbl_stat_src_serial_num_stat_date_data_type ON public.tbl_stat_src (serial_num, stat_date, data_type);
 
 -- add comment for index
-COMMENT ON INDEX public.idx_tbl_stat_src_stat_date_data_type_serial_num IS 'index for statistic query by stat_date, data_type, serial_num';
-
+COMMENT ON INDEX public.idx_tbl_stat_src_serial_num_stat_date_data_type IS 'index for statistic query by serial_num, stat_date, data_type';
