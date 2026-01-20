5f982fb49e031e99784644d608b7a17a26a95983
taekyuzzz <hongtk@ckstack.com>
2026-01-20 20:47:03 +0900

    stat_src3 추가. (실질 stat insert 테스트용)

--- _assets/sql/ck_stat_db/create_table.sql
+++ _assets/sql/ck_stat_db/create_table.sql
@@ -63,3 +63,36 @@ CREATE INDEX idx_serial_num_stat_date_data_type ON public.tbl_stat_src2 (serial_
 
 -- add comment for index
 COMMENT ON INDEX public.idx_serial_num_stat_date_data_type IS 'index for statistic query by serial_num, stat_date, data_type';
+
+
+/**
+ * tbl_stat_src3
+ * stat source raw table (backup/sync capable)
+ */
+-- stat src
+-- DROP TABLE public.tbl_stat_src;
+-- TODO partition
+-- create table
+CREATE TABLE public.tbl_stat_src3 (
+  stat_date  CHAR(14) NOT NULL,
+  serial_num CHAR(17) NOT NULL,
+  data_type  CHAR(32) NOT NULL,
+  value      INTEGER,
+  fvalue     INTEGER
+);
+
+-- add comment for column
+COMMENT ON TABLE public.tbl_stat_src3 IS 'stat source raw table';
+COMMENT ON COLUMN public.tbl_stat_src3.stat_date IS 'statistic date. YYYYMMDDHHmmss';
+COMMENT ON COLUMN public.tbl_stat_src3.serial_num IS 'boiler serial number';
+COMMENT ON COLUMN public.tbl_stat_src3.data_type IS 'statistic type';
+COMMENT ON COLUMN public.tbl_stat_src3.value IS 'statistic value or integer part of statistic value';
+COMMENT ON COLUMN public.tbl_stat_src3.value IS 'fractional part of statistic value';
+
+-- TODO index query 별도 분리
+-- ad index
+-- 전체 장비도 조회할 수 있으니 일단 순서를 data_type 에 serial_num 으로 변경
+CREATE INDEX idx_serial_num_stat_date_data_type3 ON public.tbl_stat_src3 (serial_num, stat_date, data_type);
+
+-- add comment for index
+COMMENT ON INDEX public.idx_serial_num_stat_date_data_type3 IS 'index for statistic query by serial_num, stat_date, data_type';
